1bad9de70537e291eda5e8964627c26ff912c090
fscarlato <scarlato@link.it>
2020-09-14 14:34:22 +0200

    Aggiunti in testsuite.properties i valori <connect_timeut> e <read_timeout>

--- protocolli/modipa/testsuite/src/karate-config.js
+++ protocolli/modipa/testsuite/src/karate-config.js
@@ -11,8 +11,11 @@ function fn() {
     // customize
   }
 
-  karate.configure('connectTimeout',  1000000);
-  karate.configure('readTimeout', 1000000);
+  connect_timeout = karate.properties["connect_timeout"] || 1000000
+  read_timeout = karate.properties["read_timeout"] || 1000000
+
+  karate.configure('connectTimeout',  connect_timeout);
+  karate.configure('readTimeout', read_timeout);
   
   return { 
     govway_base_path: karate.properties["govway_base_path"]
