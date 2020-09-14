e77d7402644df24fc8ad6926cfa43a55cd309202
Andrea Poli <andreapoli80@gmail.com>
2020-09-14 14:32:45 +0200

    Corrette classi java della testsuite

--- protocolli/modipa/testsuite/src/org/openspcoop2/core/protocolli/ConfigLoader.java
+++ protocolli/modipa/testsuite/src/org/openspcoop2/core/protocolli/ConfigLoader.java
@@ -1,22 +1,45 @@
-package org.openspcoop2.core.protocolli.modipa.testsuite;
+/*
+ * GovWay - A customizable API Gateway 
+ * https://govway.org
+ * 
+ * Copyright (c) 2005-2020 Link.it srl (https://link.it). 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
-import com.intuit.karate.FileUtils;
-import com.intuit.karate.KarateOptions;
-import com.intuit.karate.junit4.Karate;
-import com.intuit.karate.netty.FeatureServer;
+package org.openspcoop2.core.protocolli;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import java.util.Map;
-import java.util.Properties;
-import java.util.HashMap;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.IOException;
-import org.openspcoop2.utils.UtilsException;
 import org.openspcoop2.utils.LoggerWrapperFactory;
+import org.openspcoop2.utils.UtilsException;
 
+import com.intuit.karate.FileUtils;
 
+/**
+* ConfigLoader
+*
+* @author Francesco Scarlato (scarlato@link.it)
+* @author $Author$
+* @version $Rev$, $Date$
+*/
 public class ConfigLoader {
 
     private static final String propFileName = "testsuite.properties";
@@ -29,40 +52,42 @@ public class ConfigLoader {
     @BeforeClass
     public static void prepareConfig() throws FileNotFoundException, IOException, UtilsException, Exception {
 
-        InputStream inputStream = ConfigLoader.class.getClassLoader().getResourceAsStream(propFileName);
+        try(InputStream inputStream = ConfigLoader.class.getClassLoader().getResourceAsStream(propFileName);){
  
-        if (inputStream != null) {
-            prop.load(inputStream);
-        } else {
-            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+	        if (inputStream != null) {
+	            prop.load(inputStream);
+	        } else {
+	            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+	        }
+	
+	        org.slf4j.Logger logger = LoggerWrapperFactory.getLogger("com.intuit.karate");
+	
+	        String configLoaderPath = prop.getProperty("config_loader_path");
+	        String scriptPath = configLoaderPath + "/" + (FileUtils.isOsWindows() ? "createOrUpdate.cmd" : "createOrUpdate.sh");
+	        logger.debug("Config loader path: " + scriptPath);    // TODO: Usare il logger
+	
+	        String modipaBundle = new File(modipaBundlePath).getAbsolutePath();
+	        logger.debug("ModIPA bundle path: " + modipaBundle);
+	        
+	        logger.debug("Carico la configurazione su govway...");
+	        org.openspcoop2.utils.resources.ScriptInvoker scriptInvoker = new org.openspcoop2.utils.resources.ScriptInvoker(scriptPath);
+	        scriptInvoker.run(new File(configLoaderPath), modipaBundle);
+	
+	        // Dopo aver caricato lo script, resetto le cache
+	        String[] govwayCaches = prop.getProperty("jmx_cache_resources").split(",");
+	        for (String resource : govwayCaches) {
+	            logger.debug("Resetto cache: " + resource);
+	            String url = prop.getProperty("govway_base_path") + "/check?methodName=resetCache&resourceName=" + resource;
+	            org.openspcoop2.utils.transport.http.HttpUtilities.check(
+	                url,
+	                prop.getProperty("jmx_cache_username"),
+	                prop.getProperty("jmx_cache_password")
+	            );
+	        }
+	
+	        System.setProperty("govway_base_path", prop.getProperty("govway_base_path"));
+	        
         }
-
-        org.slf4j.Logger logger = LoggerWrapperFactory.getLogger("com.intuit.karate");
-
-        String configLoaderPath = prop.getProperty("config_loader_path");
-        String scriptPath = configLoaderPath + "/" + (FileUtils.isOsWindows() ? "createOrUpdate.cmd" : "createOrUpdate.sh");
-        logger.debug("Config loader path: " + scriptPath);    // TODO: Usare il logger
-
-        String modipaBundle = new File(modipaBundlePath).getAbsolutePath();
-        logger.debug("ModIPA bundle path: " + modipaBundle);
-        
-        logger.debug("Carico la configurazione su govway...");
-        org.openspcoop2.utils.resources.ScriptInvoker scriptInvoker = new org.openspcoop2.utils.resources.ScriptInvoker(scriptPath);
-        scriptInvoker.run(new File(configLoaderPath), modipaBundle);
-
-        // Dopo aver caricato lo script, resetto le cache
-        String[] govwayCaches = prop.getProperty("jmx_cache_resources").split(",");
-        for (String resource : govwayCaches) {
-            logger.debug("Resetto cache: " + resource);
-            String url = prop.getProperty("govway_base_path") + "/check?methodName=resetCache&resourceName=" + resource;
-            org.openspcoop2.utils.transport.http.HttpUtilities.check(
-                url,
-                prop.getProperty("jmx_cache_username"),
-                prop.getProperty("jmx_cache_password")
-            );
-        }
-
-        System.setProperty("govway_base_path", prop.getProperty("govway_base_path"));
     }
 
     @AfterClass
