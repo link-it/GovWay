74b6f04313e003d45e5b894f1128fa8aae0ca824
fscarlato <scarlato@link.it>
2020-11-24 14:45:26 +0100

    wip

--- protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/rate_limiting/occupazione_banda/RestTest.java
+++ protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/rate_limiting/occupazione_banda/RestTest.java
@@ -1,4 +1,4 @@
-package org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.completate_con_successo;
+package org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.occupazione_banda;
 
 import java.util.Vector;
 
@@ -14,14 +14,16 @@ import org.openspcoop2.utils.transport.http.HttpUtilsException;
 
 public class RestTest extends ConfigLoader {
 	
+	private static final int requestSizeBytes = 1024;
+	
 	@Test
 	public void perMinutoErogazione() throws UtilsException, HttpUtilsException, InterruptedException {
 		final int maxRequests = 5;
 
-		String idPolicy = dbUtils.getIdPolicyErogazione("SoggettoInternoTest", "RichiesteCompletateConSuccessoRest", PolicyAlias.MINUTO);
+		String idPolicy = dbUtils.getIdPolicyErogazione("SoggettoInternoTest", "OccupazioneBandaRest", PolicyAlias.MINUTO);
 		Utils.resetCounters(idPolicy);
 		
-		idPolicy = dbUtils.getIdPolicyErogazione("SoggettoInternoTest", "RichiesteCompletateConSuccessoRest", PolicyAlias.MINUTO);
+		idPolicy = dbUtils.getIdPolicyErogazione("SoggettoInternoTest", "OccupazioneBandaRest", PolicyAlias.MINUTO);
 		// Utils.checkPreConditionsNumeroRichieste(idPolicy); TODO
 		
 		// Aspetto lo scoccare del minuto
@@ -30,13 +32,23 @@ public class RestTest extends ConfigLoader {
 		
 		HttpRequest request = new HttpRequest();
 		request.setContentType("application/json");
-		request.setMethod(HttpRequestMethod.GET);
-		request.setUrl( System.getProperty("govway_base_path") + "/SoggettoInternoTest/RichiesteCompletateConSuccessoRest/v1/minuto");
+		request.setContent(generatePayload(requestSizeBytes));
+		request.setMethod(HttpRequestMethod.POST);
+		request.setUrl( System.getProperty("govway_base_path") + "/SoggettoInternoTest/OccupazioneBandaRest/v1/minuto");
 						
 		Vector<HttpResponse> responses = Utils.makeParallelRequests(request, maxRequests + 1);
 		
-		// Utils.checkPostConditionsNumeroRichieste(idPolicy, maxRequests); TODO		
+		System.out.println("Risposta: " + new String(responses.get(0).getContent()));
 		
+		// Utils.checkPostConditionsNumeroRichieste(idPolicy, maxRequests); TODO				
+	}
+
+	private byte[] generatePayload(int payloadSize) {		
+		byte[] ret = new byte[payloadSize];
+		for (int i=0;i<payloadSize;i++) {
+			ret[i] = 97;
+		}
+		return ret;
 	}
 
 }
