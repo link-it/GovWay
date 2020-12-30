c2022487d72fe6f042808872b3bf03d484325a69
fscarlato <scarlato@link.it>
2020-12-30 16:39:07 +0100

    wip

--- protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/rate_limiting/global_policy/RestTest.java
+++ protocolli/trasparente/testsuite/karate/src/org/openspcoop2/core/protocolli/trasparente/testsuite/rate_limiting/global_policy/RestTest.java
@@ -27,6 +27,7 @@ import org.junit.Test;
 import org.openspcoop2.core.protocolli.trasparente.testsuite.ConfigLoader;
 import org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.TipoServizio;
 import org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.Utils;
+import org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.Utils.PolicyAlias;
 import org.openspcoop2.utils.transport.http.HttpRequest;
 import org.openspcoop2.utils.transport.http.HttpRequestMethod;
 import org.openspcoop2.utils.transport.http.HttpResponse;
@@ -40,12 +41,14 @@ public class RestTest extends ConfigLoader {
 	
 	
 	static void testGlobalPolicy(TipoServizio tipoServizio) {
-		// Usiamo l'erogazione già presente per il test numero_richieste,
+		// Usiamo l'erogazione già presente per il test numero_richieste
 		// visto che li ci sono policy sul numero di richieste parallele passando lo header
-		// Così vedo anche che succede agli headers quando c'è una policy globale+policy locale
-
 		// GovWay-TestSuite-RL-GlobalPolicy=Orario per attivare la policy globale
+		// vedo anche che succede agli headers quando c'è una policy globale+policy locale
+
+
 		final int maxRequests = 5;
+		final int windowSize = Utils.getPolicyWindowSize(PolicyAlias.ORARIO);
 		Utils.waitForNewHour();
 		
 		HttpRequest request = new HttpRequest();
@@ -57,7 +60,8 @@ public class RestTest extends ConfigLoader {
 		Vector<HttpResponse> responseOk = Utils.makeSequentialRequests(request, maxRequests);
 		Vector<HttpResponse> responseBlocked = Utils.makeSequentialRequests(request, maxRequests);
 		
-		// TODO: Continua
+		org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.numero_richieste_completate_con_successo.RestTest.checkOkRequests(responseOk, windowSize, maxRequests);
+		org.openspcoop2.core.protocolli.trasparente.testsuite.rate_limiting.numero_richieste_completate_con_successo.RestTest.checkFailedRequests(responseBlocked, windowSize, maxRequests);
 		
 
 	}
